3087edb42226d9ad42dcc15368659a94f9ddd806
prasunNstu <prasunjeetsaha@gmail.com>
2024-06-13 12:27:20 +0600

    "First Commit"

---
+++ header.php
@@ -0,0 +1,30 @@
+
+<?php
+	session_start();
+?>
+<header>
+<div class="header">
+        <div class="header-item">
+            <h2>BookMyMovie</h2>
+        </div>
+
+        <?php
+						if(!$_SESSION['email'] && (!$_SESSION['pass'])){
+					?>
+        <div class="header-item">
+            <a id="menu-item" href="index.php">Home</a>
+            <!-- <a id="menu-item" href="#">Movies</a> -->
+            <a id="menu-item" href="login.php">Login</a>
+        </div>
+        <?php
+                        }else{
+                            ?>
+                            <div class="header-item">
+            <a id="menu-item" href="index.php">Home</a>
+            <!-- <a id="menu-item" href="#">Movies</a> -->
+            <a id="menu-item" href="profile.php">Profile</a>
+            <a id="menu-item" href="login.php?logstate=logout">Logout</a>
+        </div>
+                  <?php      } ?>
+    </div>
+                        </header>
